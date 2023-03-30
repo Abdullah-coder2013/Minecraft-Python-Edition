0b09c120d032632898edd9b67f9075706d717196
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2023-03-30 16:31:47 +0200

    update 1.7

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ window.show_ursina_splash = True
 window.title = "Minecraft Python Edition"
 player.height = 2
 player.gravity = 0.5
+# player.model = "assets/player.obj"
+# player.texture = "textures/"
+terrainblocks = []
+player.scale = 1
 player.y = 50
 block_id = 1
 bgmusic = [Audio("assets/music/calm1.ogg", loop=True, autoplay=False),
@@ -35,7 +39,7 @@ voxels = []
 noise = PerlinNoise(octaves=3,seed=random.randint(1,1000000))
 
 blocks = [
-    ['hello', "hello", "hello"],
+    ['leaves', "assets/leaves_block_tex.png", load_texture("assets/leaves_block_tex.png")],
     ["grass", "assets/grass_block_tex.png", load_texture("assets/grass_block_tex.png")],
     ["dirt", "assets/dirt_block_tex.png", load_texture("assets/dirt_block_tex.png")],
     ["stone", "assets/stone_block_tex.png", load_texture("assets/stone_block_tex.png")],
@@ -47,15 +51,70 @@ blocks = [
     ["ice", "assets/ice_block_tex.png", load_texture("assets/ice_block_tex.png")]
 ]
 
-def findsoundbasedontexture(blockid,blocklist=blocks):
-    if blocklist[blockid][0] in blocklist[blockid][1]:
-        sound = Audio(f"assets/sounds/{blocklist[blockid][0]}/{blocklist[blockid][0]}{random.randint(1, 4)}", loop=False, autoplay=False)
-        return sound
+
+def trunk(parent):
+    for __z in range(1):
+        for __x in range(1):
+            for __y in range(3):
+                voxel = Voxel(position=(__x, __y - 3, __z), texture=blocks[6][2])
+                voxel.parent = parent
+
+    for ___z in range(5):
+        for ___x in range(5):
+            for ___y in range(2):
+                voxel = Voxel(position=(___x - 2, ___y, ___z - 2),
+                              texture='assets/leaves_block_tex.png')
+                voxel.parent = parent
+                
+    for ____z in range(3):
+        for ____x in range(3):
+            for ____y in range(2):
+                voxel = Voxel(position=(____x - 1, ____y+2, ____z - 1),
+                              texture='assets/leaves_block_tex.png')
+                voxel.parent = parent
+
+def plantTree(_x, _y, _z):
+    tree = Entity(model=None, position=Vec3(_x, _y, _z))
+    trunk(tree)
+    tree.y = 3
+
+
+def checkTree(_x, _y, _z):
+    freq = 3
+    amp = 80
+    treeChance = ((noise([_x / freq, _z / freq])) * amp)
+    if treeChance > 38:
+        plantTree(_x, _y, _z)
+
+
+def genTrees():
+    for tree in range(5):
+        chosenblock = random.choice(terrainblocks)
+        randomcoordinates = (random.randint(-10,10),random.randint(-10,10),chosenblock.y)
+        plantTree(_x=randomcoordinates[0], _z=randomcoordinates[1], _y=randomcoordinates[2]+1)
+
+def findsoundbasedontexture(blockid, mode, block, blocklist=blocks):
+    if mode == "default":
+        if blocklist[blockid][0] in blocklist[blockid][1]:
+            sound = Audio(f"assets/sounds/{blocklist[blockid][0]}/{blocklist[blockid][0]}{random.randint(1, 4)}", loop=False, autoplay=False)
+            return sound
+    elif mode == "already":
+        for blockk in blocklist:
+            if blockk[0] == block:
+                soundd = Audio(f"assets/sounds/{block}/{block}{random.randint(1,4)}", loop=False, autoplay=False)
+                return soundd
     
-# def whichblockami(block):
-#     for eachBlock in blocks:
-#         if block.texture in eachBlock[1]:
-#             print(eachBlock[0])
+def whichblockami(block):
+    for eachBlock in blocks:
+        try:
+            if f"assets/{block.texture.name}" in eachBlock[1]:
+                print(eachBlock[0])
+                return eachBlock[0]
+        
+        except:
+            print(f"No texture on index")
+            
+
         
         
     
@@ -69,7 +128,10 @@ def update():
     if held_keys['escape']:
         sys.exit()
         
-    if player.y < -110:
+    if held_keys["t"]:
+        plantTree(round(player.x), round(player.y), round(player.z))
+        
+    if player.y < -100:
         Audio("assets/sounds/sh/die.ogg")
         player.y = 15
         player.x = 0
@@ -99,18 +161,19 @@ class Voxel(Button):
             texture=texture,
             color=color.color(0, 0, random.uniform(0.9, 1)),
             scale=1,
-            shader=basic_lighting_shader,
-            # block=whichblockami(self)
+            shader=basic_lighting_shader
         )
+        
+        self.block = whichblockami(self)
 
     def input(self, key):
         if self.hovered:
             if key == 'right mouse down':
-                findsoundbasedontexture(blockid=block_id).play()
+                findsoundbasedontexture(blockid=block_id,mode="default",block=self.block).play()
                 Voxel(position=self.position + mouse.normal,
                       texture=blocks[block_id][2])
             if key == 'left mouse down':
-                findsoundbasedontexture(blockid=block_id).play()
+                findsoundbasedontexture(blockid=block_id,mode="already",block=self.block).play()
                 destroy(self)
 
     def create_sides(self, direction, x, y, z):
@@ -172,10 +235,10 @@ for z in range(-10,10):
         y = noise([x * .02, z * .02])
         y = math.floor(y * 7.5)
         voxel = Voxel(position=(x,y,z))
-        # voxels.append(voxel)
+        terrainblocks.append(voxel)
         
 hand = Hand()
-
+genTrees()
 app.run()
 
 
